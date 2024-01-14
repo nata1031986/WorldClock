f4c93c79cfce6e268250c9d088d9de35cb016291
Tasha Perry <nata1031986@gmail.com>
2024-01-14 22:32:14 +0200

    grtt

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function updateTime() {
-  let chicagoElement = document.querySelector("#chicago");
-  if (chicagoElement) {
-    let chicagoDateElement = chicagoElement.querySelector(".date");
-    let chicagoTimeElement = chicagoElement.querySelector(".time");
-    let chicagoTime = moment().tz("America/Chicago");
+    let chicagoElement = document.querySelector("#chicago");
+    if (chicagoElement) {
+        let chicagoDateElement = chicagoElement.querySelector(".date");
+        let chicagoTimeElement = chicagoElement.querySelector(".time");
+        let chicagoTime = moment().tz("America/Chicago");
 
-    chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
-    chicagoTimeElement.innerHTML = chicagoTime.format("h:mm:ss [<small>]A[</small>]");
-  }
+        chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+        chicagoTimeElement.innerHTML = chicagoTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 
-  let kievElement = document.querySelector("#kiev");
-  if (kievElement) {
-    let kievDateElement = kievElement.querySelector(".date");
-    let kievTimeElement = kievElement.querySelector(".time");
-    let kievTime = moment().tz("Europe/Kiev");
+    let kievElement = document.querySelector("#kiev");
+    if (kievElement) {
+        let kievDateElement = kievElement.querySelector(".date");
+        let kievTimeElement = kievElement.querySelector(".time");
+        let kievTime = moment().tz("Europe/Kiev");
 
-    kievDateElement.innerHTML = kievTime.format("MMMM Do YYYY");
-    kievTimeElement.innerHTML = kievTime.format("h:mm:ss [<small>]A[</small>]");
-  }
+        kievDateElement.innerHTML = kievTime.format("MMMM Do YYYY");
+        kievTimeElement.innerHTML = kievTime.format("h:mm:ss [<small>]A[</small>]");
+    }
 }
 
 let citiesSelectElement = document.querySelector("#citySelect"); 
@@ -25,4 +25,3 @@ citiesSelectElement.addEventListener("change", updateTime);
 
 updateTime();
 setInterval(updateTime, 1000);
-
