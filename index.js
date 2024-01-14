43fd2ee89fda54b90b62a80d49e4542cbf5f1120
Tasha Perry <nata1031986@gmail.com>
2024-01-14 22:01:43 +0200

    fixed some issues

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-function updateTime () {
-    let kyivElement = document.querySelector("#Kyiv");
-  let kyivDateElement = kyivElement.querySelector(".date");
-  let kyivTimeElement = kyivElement.querySelector(".time");
-  let kyivTime = moment(). tz("Europe/Kyiv");
+function updateTime() {
+ 
+  let chicagoElement = document.querySelector("#chicago");
+  if (chicagoElement) {
+    let chicagoDateElement = chicagoElement.querySelector(".date");
+    let chicagoTimeElement = chicagoElement.querySelector(".time");
+    let chicagoTime = moment().tz("America/Chicago");
 
-  kyivDateElement.innerHTML = moment(). format("MMMM Do YYYY");
-  kyivTimeElement.innerHTML = kyivTime.format("h:mm:ss [<small>]A[</small>]");
+    chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+    chicagoTimeElement.innerHTML = chicagoTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 
 
+  let kievElement = document.querySelector("#kiev");
+  if (kievElement) {
+    let kievDateElement = kievElement.querySelector(".date");
+    let kievTimeElement = kievElement.querySelector(".time");
+    let kievTime = moment().tz("Europe/Kiev");
 
-  let chicagoElement = document.querySelector("#Chicago");
-  let chicagoDateElement = chicagoElement.querySelector(".date");
-  let chicagoTimeElement = chicagoElement.querySelector(".time");
-  let chicagoTime = moment().tz("America/Chicago");
-
-  chicagoDateElement.innerHTML = moment(). format("MMMM Do YYYY");
-  chicagoTimeElement.innerHTML = chicagoTime.format("h:mm:ss [<small>]A[</small>]");
+    kievDateElement.innerHTML = kievTime.format("MMMM Do YYYY");
+    kievTimeElement.innerHTML = kievTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 }
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
 updateTime();
-setInterval(updateTime,1000);
+setInterval(updateTime, 1000);
