f4feea43a7e73a551e5416b2ce5961c08e553b1c
Tasha Perry <nata1031986@gmail.com>
2024-01-16 13:18:58 +0200

    improved js

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-  function updateTime() {
-    let chicagoElement = document.querySelector("#chicago");
-    if (chicagoElement) {
-        let chicagoDateElement = chicagoElement.querySelector(".date");
-        let chicagoTimeElement = chicagoElement.querySelector(".time");
-        let chicagoTime = moment().tz("America/Chicago");
+function updateTime() {
 
-        chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
-        chicagoTimeElement.innerHTML = chicagoTime.format("h:mm:ss [<small>]A[</small>]");
-    }
+  let chicagoElement = document.querySelector("#chicago");
+  if (chicagoElement) {
+    let chicagoDateElement = chicagoElement.querySelector(".date");
+    let chicagoTimeElement = chicagoElement.querySelector(".time");
+    let chicagoTime = moment().tz("America/Chicago");
 
-    let kievElement = document.querySelector("#kiev");
-    if (kievElement) {
-        let kievDateElement = kievElement.querySelector(".date");
-        let kievTimeElement = kievElement.querySelector(".time");
-        let kievTime = moment().tz("Europe/Kiev");
-
-        kievDateElement.innerHTML = kievTime.format("MMMM Do YYYY");
-        kievTimeElement.innerHTML = kievTime.format("h:mm:ss [<small>]A[</small>]");
-    }
+    chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+    chicagoTimeElement.innerHTML = chicagoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
   }
 
-  let citiesSelectElement = document.querySelector("#citiesSelect");
-  citiesSelectElement.addEventListener("change", updateTime);
 
-  updateTime();
-  setInterval(updateTime, 1000);
-});
+  let kievElement = document.querySelector("#kiev");
+  if (kievElement) {
+    let kievDateElement = kievElement.querySelector(".date");
+    let kievTimeElement = kievElement.querySelector(".time");
+    let kievTime = moment().tz("Europe/Kiev");
+
+    kievDateElement.innerHTML = kievTime.format("MMMM Do YYYY");
+    kievTimeElement.innerHTML = kievTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+    </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
